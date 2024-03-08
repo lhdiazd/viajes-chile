50ac2a42ddfa4f738db2193b49fa1f14261f184d
luis <luis.hernan.diaz.d@gmail.com>
2024-03-08 17:08:13 -0300

    añade comentarios a el archivo script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,27 @@
 $(document).ready(function () {
+    // Muestra el modal con el ID "exampleModal" al cargar la página
     $('#exampleModal').modal('show');
 
-    $('[data-bs-toggle="tooltip"]').tooltip();   
+    // Activa el tooltip de Bootstrap para elementos con el atributo "data-bs-toggle" igual a "tooltip"
+    $('[data-bs-toggle="tooltip"]').tooltip();
 
+    // Asigna un controlador de eventos a todos los enlaces en la página
     $("a").on('click', function (event) {
+        // Verifica si el enlace tiene un valor de fragmento en su URL
         if (this.hash !== "") {
+            // Evita el comportamiento predeterminado del enlace para evitar que la página se cargue nuevamente
             event.preventDefault();
+
+            // Almacena el valor del fragmento del enlace en la variable "hash"
             var hash = this.hash;
+
+            // Realiza una animación de desplazamiento suave hasta el elemento en la página identificado por el fragmento del enlace
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 1000, function () {
-
+            }, 1000, function () {                
                 window.location.hash = hash;
             });
         }
     });
-
-    
-
-    
 });
 
